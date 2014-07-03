e4a4d733ef2f8feb10f1423542e6cd6c64dd0314
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-07-03 20:53:07 +0200

    Add _generate_basic_test()

--- cpanfile
+++ cpanfile
@@ -3,4 +3,6 @@ requires "Applify" => "0.06";
 requires "File::Path" => "0";
 requires "CPAN::Uploader" => "0.10";
 requires "Module::Find" => "0.10";
+requires "Test::Pod" => "0";
+requires "Test::Pod::Coverage" => "0";
 test_requires "Test::More" => "0.90";
--- lib/App/git/ship/perl.pm
+++ lib/App/git/ship/perl.pm
@@ -183,6 +183,7 @@ sub init {
   symlink $self->main_module_path, 'README.pod' unless -e 'README.pod';
 
   $self->SUPER::init(@_);
+  $self->_generate_basic_test;
   $self->_generate_changes;
   $self->_generate_manifest_skip;
   $self;
@@ -248,6 +249,47 @@ sub _dist_files {
   }
 
   return undef;
+}
+
+sub _generate_basic_test {
+  my $self = shift;
+  my $file = File::Spec->catfile(qw( t 00-basic.t ));
+
+  mkdir 't' unless -d 't';
+  return if -e $file;
+  open my $TEST, '>', $file or $self->abort("Could not write to $file: $!");
+  print $TEST <<'TEST';
+use Test::More;
+use File::Find;
+
+if(($ENV{HARNESS_PERL_SWITCHES} || '') =~ /Devel::Cover/) {
+  plan skip_all => 'HARNESS_PERL_SWITCHES =~ /Devel::Cover/';
+}
+if(!eval 'use Test::Pod; 1') {
+  *Test::Pod::pod_file_ok = sub { SKIP: { skip "pod_file_ok(@_) (Test::Pod is required)", 1 } };
+}
+if(!eval 'use Test::Pod::Coverage; 1') {
+  *Test::Pod::Coverage::pod_coverage_ok = sub { SKIP: { skip "pod_coverage_ok(@_) (Test::Pod::Coverage is required)", 1 } };
+}
+
+find(
+  {
+    wanted => sub { /\.pm$/ and push @files, $File::Find::name },
+    no_chdir => 1
+  },
+  -e 'blib' ? 'blib' : 'lib',
+);
+
+plan tests => @files * 3;
+
+for my $file (@files) {
+  my $module = $file; $module =~ s,\.pm$,,; $module =~ s,.*/?lib/,,; $module =~ s,/,::,g;
+  ok eval "use $module; 1", "use $module" or diag $@;
+  Test::Pod::pod_file_ok($file);
+  Test::Pod::Coverage::pod_coverage_ok($module);
+}
+TEST
+
 }
 
 sub _generate_changes {
