6ba315fda7ea0f759d8f6b04d6dabc70ee72c99b
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-07-31 12:17:50 +0200

    Released version 0.09
           * Try to avoid failing cpantesters test "!! 'git init' failed: 1"

--- lib/App/git/ship.pm
+++ lib/App/git/ship.pm
@@ -6,7 +6,7 @@ App::git::ship - Git command for shipping your project
 
 =head1 VERSION
 
-0.08
+0.09
 
 =head1 DESCRIPTION
 
@@ -86,7 +86,7 @@ use File::Spec ();
 
 use constant DEBUG => $ENV{GIT_SHIP_DEBUG} || 0;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 my %DATA;
 
