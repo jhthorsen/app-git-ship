24287b0d789541b996973c9cdc019b6a48f4f0ea
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-04-20 09:01:26 +0200

    Released version 0.19
     - Fix double newline in Changes

--- lib/App/git/ship.pm
+++ lib/App/git/ship.pm
@@ -6,7 +6,7 @@ App::git::ship - Git command for shipping your project
 
 =head1 VERSION
 
-0.18
+0.19
 
 =head1 DESCRIPTION
 
@@ -228,7 +228,7 @@ use IPC::Run3 ();
 
 use constant DEBUG => $ENV{GIT_SHIP_DEBUG} || 0;
 
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 
 my %DATA;
 
