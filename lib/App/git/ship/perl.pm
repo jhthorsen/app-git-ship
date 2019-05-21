99f2ebc04cd473634753244defa2509b4511335a
Rolf Stöckli <tekki@cpan.org>
2019-05-21 18:16:33 +0200

    Make basic test strict.

--- lib/App/git/ship/perl.pm
+++ lib/App/git/ship/perl.pm
@@ -740,6 +740,11 @@ WriteMakefile(%WriteMakefileArgs);
 ^\.perltidyrc
 ^\.travis.yml
 @@ t/00-basic.t
+use strict;
+use warnings;
+use utf8;
+## no critic (StringyEval)
+
 use Test::More;
 use File::Find;
 
@@ -756,6 +761,7 @@ if(!eval 'use Test::CPAN::Changes; 1') {
   *Test::CPAN::Changes::changes_file_ok = sub { SKIP: { skip "changes_ok(@_) (Test::CPAN::Changes is required)", 4 } };
 }
 
+my @files;
 find(
   {
     wanted => sub { /\.pm$/ and push @files, $File::Find::name },
